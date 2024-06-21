4408dc75550b69897ccdb3a7e066b985cc04ba96
Dilip <37419699+dildeo1@users.noreply.github.com>
2024-06-21 07:49:04 +0530

    Update main.py

--- web-app-demo/Backend/main.py
+++ web-app-demo/Backend/main.py
@@ -1,30 +1,18 @@
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-""" Backend API that handles DocAI API calls """
 import os
 from typing import Dict
+import logging
 
 from api.helper import populate_list_source
 from api.helper import process_document
 from api.helper import store_file
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
 from flask_cors import CORS  # comment this on deployment
 from flask_restful import Api
 import google.auth
+
+# Setup logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 _, project_id = google.auth.default()
 LOCATION = "us"  # Format is 'us' or 'eu'
@@ -32,54 +20,59 @@ LOCATION = "us"  # Format is 'us' or 'eu'
 processor_id_by_processor_type: Dict[str, str] = {}
 
 app = Flask(__name__, static_url_path="", static_folder="")
-
 CORS(app)
 api = Api(app)
+
 @app.route("/", methods=["GET"])
 def home():
     """Gets all available processors that are in the specified GCP project"""
-
+    logger.info("Home endpoint called")
     populate_list_source(project_id, LOCATION, processor_id_by_processor_type)
+    logger.info("Processor list populated")
+    print("Processor list populated")
     return jsonify(
         {
             "resultStatus": "SUCCESS",
         }
     )
-
 
 @app.route("/api/init", methods=["GET"])
 def populate_list():
     """Gets all available processors that are in the specified GCP project"""
-
+    logger.info("Populate list endpoint called")
     populate_list_source(project_id, LOCATION, processor_id_by_processor_type)
+    logger.info("Processor list populated")
     return jsonify(
         {
             "resultStatus": "SUCCESS",
         }
     )
 
-
 @app.route("/api/docai", methods=["POST"])
 def get_document():
     """Calls process_document and returns document proto"""
-
+    logger.info("DocAI endpoint called")
     directory = "api/test_docs"
     for file in os.listdir(directory):
         os.remove(os.path.join(directory, file))
+    logger.info("Cleared test_docs directory")
 
     processor_type = request.form["fileProcessorType"]
+    logger.info(f"Received processor type: {processor_type}")
 
     populate_list_source(project_id, LOCATION, processor_id_by_processor_type)
-
     processor_id = processor_id_by_processor_type.get(processor_type)
-    print(processor_id)
+    logger.info(f"Processor ID: {processor_id}")
 
     file = request.files["file"]
     file_type = file.content_type
+    logger.info(f"Received file of type: {file_type}")
 
     try:
         _destination = store_file(file)
+        logger.info(f"Stored file at: {_destination}")
     except Exception as err:  # pylint: disable=W0703
+        logger.error(f"Error storing file: {err}")
         return {
             "resultStatus": "ERROR",
             "errorMessage": str(err),
@@ -93,18 +86,19 @@ def get_document():
         "processor_id": processor_id,
     }
 
-    return process_document(process_document_request)
-
+    response = process_document(process_document_request)
+    logger.info("Processed document")
+    return response
 
 @app.route("/api/processor/list", methods=["GET"])
 def get_list():
     """Returns list of available processors"""
-
+    logger.info("Processor list endpoint called")
     processor_list = list(processor_id_by_processor_type.keys())
     response = jsonify({"resultStatus": "SUCCESS", "processor_list": processor_list})
-
+    logger.info(f"Processor list: {processor_list}")
     return response
 
-
 if __name__ == "__main__":
+    logger.info("Starting Flask application")
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
